a4247f74068e23d2cb44efbb00850a2fd6c12e1a
Kamilosb <Kamilosb.kk@gmail.com>
2023-03-08 14:09:41 +0100

    asd

--- cookieAuth.js
+++ cookieAuth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 exports.cookieAuth = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
     // console.log(req.headers)
     try {
+        const token = req.headers.authorization.split(" ")[1];
         const user = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = user
         next()
